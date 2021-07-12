b60c487fef8f05893d6e1c1d40f983483dbac7e0
mahivkpatel <mahivkpatel@gmail.com>
2021-07-12 21:28:06 +0530

    changes for the header

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,12 +1,13 @@
 import "./Header.scss";
+
 function Header() {
     return (
       <header>
         <div className="name">
           <h1>Mohini Patel</h1>
           <ul className="intro_link">
-            <li>mohinishavati@gmail.com</li>
-            <li>https://www.linkedin.com/in/mohini-patel-0870a6127/</li>
+            <li><a href="mailto:mohinishavati@gmail.com">mohinishavati@gmail.com</a></li>
+            <li><a href="https://www.linkedin.com/in/mohini-patel-0870a6127/" target="_blank">https://www.linkedin.com/in/mohini-patel-0870a6127/</a></li>
           </ul>
         </div>
         <hr/>
